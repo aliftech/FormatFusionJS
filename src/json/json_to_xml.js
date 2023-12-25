27ec9251402771eec20729393ea1a10205275314
wahyukrisnaaji32 <wahyukrisnaaji32@gmail.com>
2023-12-25 15:13:39 +0700

    feat(src/json/json_to_xml.js): add json to xml function

---
+++ src/json/json_to_xml.js
@@ -0,0 +1,13 @@
+const xml = require('xml-js');
+
+
+function json_to_xml(jsonData) {
+    try {
+        lines = xml.json2xml(jsonData);
+        return lines;
+    } catch (error) {
+        throw error;
+    }
+}
+
+module.exports = json_to_xml;
