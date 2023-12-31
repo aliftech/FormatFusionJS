8ad414a259a15bb111abd30f8942beece0375359
wahyukrisnaaji32 <wahyukrisnaaji32@gmail.com>
2023-12-31 11:03:45 +0700

    build: build version 1.0.2

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const yaml_to_json = require("./dist/yaml/yaml_to_json");
-const yaml_to_xml = require("./dist/yaml/yaml_to_xml");
-const json_to_yaml = require("./dist/json/json_to_yaml");
-const json_to_xml = require("./dist/json/json_to_xml");
-const xml_to_json = require("./dist/xml/xml_to_json");
-const xml_to_yaml = require("./dist/xml/xml_to_yaml");
+const yaml_to_json = require("./src/yaml/yaml_to_json");
+const yaml_to_xml = require("./src/yaml/yaml_to_xml");
+const json_to_yaml = require("./src/json/json_to_yaml");
+const json_to_xml = require("./src/json/json_to_xml");
+const xml_to_json = require("./src/xml/xml_to_json");
+const xml_to_yaml = require("./src/xml/xml_to_yaml");
 
 module.exports = {
     yaml_to_json,
