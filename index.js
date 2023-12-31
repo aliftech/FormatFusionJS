cf3ca798d0b08ba7579a9408888f9add58fec463
wahyukrisnaaji32 <wahyukrisnaaji32@gmail.com>
2023-12-31 10:55:23 +0700

    build: build version 1.0.1

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-module.exports = require("./dist");
+const yaml_to_json = require("./dist/yaml/yaml_to_json");
+const yaml_to_xml = require("./dist/yaml/yaml_to_xml");
+const json_to_yaml = require("./dist/json/json_to_yaml");
+const json_to_xml = require("./dist/json/json_to_xml");
+const xml_to_json = require("./dist/xml/xml_to_json");
+const xml_to_yaml = require("./dist/xml/xml_to_yaml");
+
+module.exports = {
+    yaml_to_json,
+    yaml_to_xml,
+    json_to_xml,
+    json_to_yaml,
+    xml_to_json,
+    xml_to_yaml
+}
